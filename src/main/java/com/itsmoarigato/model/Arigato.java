e7bea2bde3dbf2bd30ebe05f9321ba440618390f
unknown <terahide27@gmail.com>
2015-05-20 17:08:00 +0900

    メッセージの追加を追加　明白な実装

--- src/main/java/com/itsmoarigato/model/Arigato.java
+++ src/main/java/com/itsmoarigato/model/Arigato.java
@@ -27,15 +27,19 @@ public class Arigato {
 	}
 
 	public List<Message> getMineMessages(String toUser) {
-		return jdbcTemplate.query("select * from arigato_tbl", new RowMapper<Message>(){
+		return jdbcTemplate.query("select from_user ,to_user ,subject ,message from arigato_tbl a inner join arigato_history_tbl h on (a.id = h.arigato_id)", new RowMapper<Message>(){
 			@Override
 			public Message mapRow(ResultSet rs, int rowNum) throws SQLException {
-				User fromUser = null; 
-				User toUser = null;
-				String subject = null; 
-				String contents = null; 
+				User fromUser = toUser(rs.getString("from_user")); 
+				User toUser = toUser(rs.getString("to_user"));
+				String subject = rs.getString("subject"); 
+				String contents = rs.getString("message"); 
 				List<Image> images = null;
 				return new Message(fromUser, toUser, subject, contents, images);
+			}
+
+			private User toUser(String email) {
+				return new User(email, "");//TODO nameの取得
 			}
 		});
 	}
