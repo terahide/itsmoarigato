d614d4e5b6effa7047a937f04e598a30ed5cd545
unknown <terahide27@gmail.com>
2015-05-20 17:14:45 +0900

    メッセージの追加を追加　リファクタリング

--- src/main/java/com/itsmoarigato/model/Arigato.java
+++ src/main/java/com/itsmoarigato/model/Arigato.java
@@ -19,10 +19,26 @@ public class Arigato {
 	JdbcTemplate jdbcTemplate;
 	
 	public void add(Message message){
+		int historyId = saveHistory(message);
+		int arigatoId = saveArigato(message);
+		populateArigatoIdToHistory(historyId,arigatoId);
+		//TODO 画像の扱い
+	}
+
+	private int saveHistory(Message message) {
 		jdbcTemplate.update("insert into arigato_history_tbl (from_user ,to_user ,subject ,message, created) values (?,?,?,?,sysdate)", new Object[]{message.getFromUser().getEmail(),message.getToUser().getEmail(),message.getSubject(),message.getContents()});
 		Integer historyId = jdbcTemplate.queryForObject("select max(id) from arigato_history_tbl where to_user = ?", Integer.class,new Object[]{message.getToUser().getEmail()});
+		return historyId;
+	}
+
+	//NOTE IDの取得がマルチトランザクションになるとうまくいかなくなる可能性がある。本質的に DBの排他制御（明示的ロック）で解決すべき問題。（シングルサーバならこれで問題ない）
+	private synchronized int saveArigato(Message message) {
 		jdbcTemplate.update("insert into arigato_tbl (created) values (sysdate)");
 		Integer arigatoId = jdbcTemplate.queryForObject("select max(id) from arigato_tbl", Integer.class);
+		return arigatoId;
+	}
+
+	private void populateArigatoIdToHistory(int historyId, int arigatoId) {
 		jdbcTemplate.update("update arigato_history_tbl set arigato_id = ? where id = ?", new Object[]{arigatoId,historyId});
 	}
 
