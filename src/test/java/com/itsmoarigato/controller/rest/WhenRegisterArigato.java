d8edd046cfcfc1d5eab8507cbc45de2e4a4d3d57
unknown <terahide27@gmail.com>
2015-05-21 16:00:04 +0900

    RESTno

--- src/test/java/com/itsmoarigato/controller/rest/WhenRegisterArigato.java
+++ src/test/java/com/itsmoarigato/controller/rest/WhenRegisterArigato.java
@@ -1,12 +1,23 @@
 package com.itsmoarigato.controller.rest;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 
 import java.net.URI;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+
+
+
+
+
+
+
+
+
 
 
 
@@ -21,9 +32,15 @@ import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itsmoarigato.Message;
 
 import util.HttpTestHelper;
 
@@ -31,12 +48,11 @@ public class WhenRegisterArigato {
 
 	@Test
 	public void test() throws Exception{
-		一覧を取得すると０件のテスト();
 		PUTするテスト();
-		一覧を取得すると1件のテスト();
-		詳細を取得するテスト();
-		POSTするテスト();
-		更新後の詳細を取得するテスト();
+		int id = 一覧を取得するとn件のテスト();
+		詳細を取得するテスト(id);
+		POSTするテスト(id);
+		更新後の詳細を取得するテスト(id);
 	}
 	private void 一覧を取得すると０件のテスト() throws Exception {
 		HashMap<String, String> params = new HashMap<>();
@@ -44,22 +60,50 @@ public class WhenRegisterArigato {
 		assertThat(response, is("[]"));
 	}
 
-	private void 一覧を取得すると1件のテスト() throws Exception {
+	private int 一覧を取得するとn件のテスト() throws Exception {
 		HashMap<String, String> params = new HashMap<>();
 		String response = get("/rest/arigato/",params);
-		assertThat(response, is("[{\"id\":193,\"fromUser\":{\"email\":\"bucho@hoge.co.jp\",\"name\":\"\"},\"toUser\":{\"email\":\"takashi@hoge.co.jp\",\"name\":\"\"},\"subject\":\"いつもありがと\",\"contents\":\"今日も頑張ってるね:)\",\"created\":\"2015-05-21\",\"images\":null}]"));
+		ObjectMapper mapper = new ObjectMapper();
+		List<Map<String,Object>> messages = mapper.readValue(response,  new TypeReference<List<Map<String,Object>>>() {});
+		assertThat(messages.size(), greaterThan(0));
+		Map<String, Object> message = messages.get(0);
+		Map<String, Object> fromUser = (Map<String, Object>)message.get("fromUser");
+		Map<String, Object> toUser = (Map<String, Object>)message.get("toUser");
+		assertThat(fromUser.get("email"), is("bucho@hoge.co.jp"));
+		assertThat(toUser.get("email"), is("takashi@hoge.co.jp"));
+		assertThat(message.get("subject"), is("いつもありがと"));
+		assertThat(message.get("contents"), is("今日も頑張ってるね:)"));
+		return (int)message.get("id");
 	}
 
-	private void 詳細を取得するテスト() throws Exception {
+	private void 詳細を取得するテスト(int id) throws Exception {
 		HashMap<String, String> params = new HashMap<>();
-		String response = get("/rest/arigato/193",params);
-		assertThat(response, is("{\"id\":193,\"fromUser\":{\"email\":\"bucho@hoge.co.jp\",\"name\":\"\"},\"toUser\":{\"email\":\"takashi@hoge.co.jp\",\"name\":\"\"},\"subject\":\"いつもありがと\",\"contents\":\"今日も頑張ってるね:)\",\"created\":\"2015-05-21\",\"images\":null}"));
+		String response = get("/rest/arigato/"+id,params);
+		ObjectMapper mapper = new ObjectMapper();
+
+		Map<String,Object> message = mapper.readValue(response,  new TypeReference<Map<String,Object>>() {});
+		Map<String, Object> fromUser = (Map<String, Object>)message.get("fromUser");
+		Map<String, Object> toUser = (Map<String, Object>)message.get("toUser");
+		assertThat(fromUser.get("email"), is("bucho@hoge.co.jp"));
+		assertThat(toUser.get("email"), is("takashi@hoge.co.jp"));
+		assertThat(message.get("subject"), is("いつもありがと"));
+		assertThat(message.get("contents"), is("今日も頑張ってるね:)"));
+		assertThat(message.get("id"), is(id));
 	}
 
-	private void 更新後の詳細を取得するテスト() throws Exception {
+	private void 更新後の詳細を取得するテスト(int id) throws Exception {
 		HashMap<String, String> params = new HashMap<>();
-		String response = get("/rest/arigato/193",params);
-		assertThat(response, is("{\"id\":193,\"fromUser\":{\"email\":\"bucho@hoge.co.jp\",\"name\":\"\"},\"toUser\":{\"email\":\"takashi@hoge.co.jp\",\"name\":\"\"},\"subject\":\"今日もありがと\",\"contents\":\"ムリしないでね:)\",\"created\":\"2015-05-21\",\"images\":null}"));
+		String response = get("/rest/arigato/"+id,params);
+		ObjectMapper mapper = new ObjectMapper();
+
+		Map<String,Object> message = mapper.readValue(response,  new TypeReference<Map<String,Object>>() {});
+		Map<String, Object> fromUser = (Map<String, Object>)message.get("fromUser");
+		Map<String, Object> toUser = (Map<String, Object>)message.get("toUser");
+		assertThat(fromUser.get("email"), is("bucho@hoge.co.jp"));
+		assertThat(toUser.get("email"), is("takashi@hoge.co.jp"));
+		assertThat(message.get("subject"), is("今日もありがと"));
+		assertThat(message.get("contents"), is("ムリしないでね:)"));
+		assertThat(message.get("id"), is(id));
 	}
 
 	private void PUTするテスト() throws Exception {
@@ -73,14 +117,14 @@ public class WhenRegisterArigato {
 		assertThat(response, is("{\"sucsses\":true}"));
 	}
 
-	private void POSTするテスト() throws Exception {
+	private void POSTするテスト(int id) throws Exception {
 		Map<String,String> params = new LinkedHashMap<>();
 		params.put("fromUserId","bucho@hoge.co.jp"); 
 		params.put("toUserId" ,"takashi@hoge.co.jp");
 		params.put("subject" ,"今日もありがと");
 		params.put("message" ,"ムリしないでね:)");
 
-		String response = post("/rest/arigato/193",params);
+		String response = post("/rest/arigato/"+id,params);
 		assertThat(response, is("{\"sucsses\":true}"));
 	}
 
