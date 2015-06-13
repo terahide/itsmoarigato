50650b37c3e6a322347e9f19f020a956c1441b69
unknown <terahide27@gmail.com>
2015-06-13 12:20:02 +0900

    DB認証に対応（roleはまだ）

--- itsmoarigato/itsmoarigato/src/main/java/com/itsmoarigato/config/SecurityConfig.java
+++ itsmoarigato/itsmoarigato/src/main/java/com/itsmoarigato/config/SecurityConfig.java
@@ -1,41 +1,112 @@
-/*
- * Copyright 2002-2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.itsmoarigato.config;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 
-/**
- * @author Rob Winch
- */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.inMemoryAuthentication()
-				.withUser("takashi@hoge.co.jp").password("password").roles("USER");
+			.userDetailsService(new UserDetailsServiceImpl())
+			.passwordEncoder(new StandardPasswordEncoder());
+//4Test			.passwordEncoder(NoOpPasswordEncoder.getInstance());
 	}
 	
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable();
 //    }
+
+	private class UserDetailsServiceImpl implements UserDetailsService	{
+		@Override
+		public UserDetails loadUserByUsername(String username)
+				throws UsernameNotFoundException {
+			
+			return jdbcTemplate.queryForObject("select email,name,password from user_Tbl where email = ?", new RowMapper_(username),new Object[]{username});
+		}
+	}
+	
+	private static class RowMapper_ implements RowMapper<UserDetails>{
+		
+		private final String username;
+		
+		public RowMapper_(String username) {
+			super();
+			this.username = username;
+		}
+
+		@Override
+		public UserDetails mapRow(ResultSet rs, int rowNum)
+				throws SQLException {
+			
+			
+			final String name = rs.getString("name");
+			final String password = rs.getString("password");
+
+			return new UserDetails() {
+				
+				@Override
+				public boolean isEnabled() {
+					return true;
+				}
+				
+				@Override
+				public boolean isCredentialsNonExpired() {
+					return true;
+				}
+				
+				@Override
+				public boolean isAccountNonLocked() {
+					return true;
+				}
+				
+				@Override
+				public boolean isAccountNonExpired() {
+					return true;
+				}
+				
+				@Override
+				public String getUsername() {
+					return username;
+				}
+				
+				public String getName() {
+					return name;
+				}
+				
+				@Override
+				public String getPassword() {
+					return password;
+				}
+				
+				@Override
+				public Collection<? extends GrantedAuthority> getAuthorities() {
+					return new ArrayList<>();
+				}
+			};
+		}
+	}
 }
+
--- itsmoarigato/itsmoarigato/src/test/java/util/TableSetuper.java
+++ itsmoarigato/itsmoarigato/src/test/java/util/TableSetuper.java
@@ -1,12 +1,18 @@
 package util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.codec.Hex;
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.util.EncodingUtils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -18,7 +24,7 @@ public class TableSetuper {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
-	@Ignore
+	//@Ignore
 	@Test
 	public void remakeTable() {
 		remakeArigatoTable();
@@ -47,6 +53,8 @@ public class TableSetuper {
 		jdbcTemplate
 				.execute("create table user_Tbl("
 						+ "email char,name varchar,password char,primary key(email))");
+		
+		registerUser();
 	}
 	private void remakeFriendTable() {
 		jdbcTemplate.execute("drop table friend_Tbl if exists");
@@ -59,10 +67,25 @@ public class TableSetuper {
 
 	private static final String me = "takashi@hoge.co.jp";
 	
+	private void registerUser(){
+		jdbcTemplate.update("delete from user_tbl where email = ?",new Object[]{me});
+		jdbcTemplate.update("delete from user_tbl where email = ?",new Object[]{Bucho.email});
+
+		registerUser(me,"takashi",e("password"));
+		registerUser(Bucho.email,"bucho",e("password"));
+	}
+	
+	private void registerUser(String email,String name,String password){
+		jdbcTemplate.update("insert into user_Tbl (email,name,password) values (?,?,?)",new Object[]{email,name,password});
+	}
+	
 	private void link(){
 		jdbcTemplate.update("delete from friend_tbl where me = ?",new Object[]{me});
 		link(me,me);
 		link(me,Bucho.email);
 	}
 	
+	static String e(String s){
+		return new StandardPasswordEncoder().encode(s);
+	}
 }
