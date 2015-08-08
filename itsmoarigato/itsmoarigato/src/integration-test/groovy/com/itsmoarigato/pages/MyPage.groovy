4496c3b700dc4a4dc972e2e67f4fde7893c446eb
terahide <terahide27@gmail.com>
2015-08-08 13:12:29 +0900

    add wait

--- itsmoarigato/itsmoarigato/src/integration-test/groovy/com/itsmoarigato/pages/MyPage.groovy
+++ itsmoarigato/itsmoarigato/src/integration-test/groovy/com/itsmoarigato/pages/MyPage.groovy
@@ -6,6 +6,7 @@ class MyPage extends Menu{
 	static at = {title == 'it\'s more early got you - mypage'}
 	static content = {
 		"最初のメッセージの更新をクリックする"{
+			waitFor{$('.edit',0) != null}
 			$('.edit',0).click()
 		}
 	} 
